95133ccb025e76890aab0f5df7026240cf4b7d9d
yasinyilmazzz <116284952+yasinyilmazzz@users.noreply.github.com>
2022-11-04 14:32:11 +0300

    Add files via upload

---
+++ day14arraysforeachloop/Exercises.java
@@ -0,0 +1,54 @@
+package day14arraysforeachloop;
+
+
+import java.util.Arrays;
+
+public class Exercises {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(firstList(1,2,3,4)));
+
+    }
+
+    public static int[] firstList(int...a) {
+        int f[]=a;
+        return f;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
