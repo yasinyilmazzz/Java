96f9a3e78b0ee9fae6b8e0d6dce4a6ab69ed23d9
Yasin YILMAZ <116284952+yasinyilmazzz@users.noreply.github.com>
2022-11-15 20:56:04 +0300

    Add files via upload

---
+++ day20accessmodifiersinheritance/Accord.java
@@ -0,0 +1,10 @@
+package day20accessmodifiersinheritance;
+
+public class Accord extends Honda{
+    public void Sport(){
+        System.out.println("Accord is in Sport Design");
+    }
+    public Accord(){
+        System.out.println("Accord class constructor");
+    }
+}
